b54032d313440d90f1efaf8ef954609b11e368ee
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-09 14:20:31 +0100

    changed page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,24 +1,29 @@
-// import {
-//   Hero,
-//   Advantages,
-//   About,
-//   Flowerpots,
-//   Goods,
-//   Gallery,
-//   Reviews,
-//   Questions,
-//   Contacts,
-// } from '@/sections';
+import {
+  Hero,
+  Advantages,
+  About,
+  Flowerpots,
+  Goods,
+  Gallery,
+  Reviews,
+  Questions,
+  Contacts,
+} from '@/sections';
 import { EXAMPLE } from '@/sections/EXAMPLE/EXAMPLE';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-green-500 text-center text-3xl font-geologica">
-        Hello Flora-Decor!!!
-      </h1>
-
+    <>
+      <Hero />
+      <Advantages />
+      <About />
+      <Flowerpots />
+      <Goods />
+      <Gallery />
+      <Reviews />
+      <Questions />
+      <Contacts />
       <EXAMPLE />
-    </div>
+    </>
   );
 }
