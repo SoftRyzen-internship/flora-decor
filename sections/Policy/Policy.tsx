c08599abd596a4aa5527b4b5c38538ce0587762e
Vita Babenko <babenkovita1988@gmail.com>
2024-04-11 14:17:25 +0300

    add navbar

--- sections/Policy/Policy.tsx
+++ sections/Policy/Policy.tsx
@@ -1,10 +1,13 @@
 import { Container } from '@/components/Container';
 import { Section } from '@/components/Section';
+import { Navbar } from '@/components/Navbar';
 
 export const Policy = () => {
   return (
     <Section variant="policySection">
-      <Container>Example Policy</Container>
+      <Container>
+        <Navbar />
+      </Container>
     </Section>
   );
 };
