c3bf422df27bf0df15d4e4dfbba045c07b78c09d
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-11 08:21:47 +0100

    fix logo styles

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -5,25 +5,24 @@ import LogoIcon from '@/public/icons/logo.svg';
 type LogoProps = {
   isHeader: boolean;
   isFooter?: boolean;
-  onClick?: () => void;
 };
 
-export const Logo = ({ isHeader, isFooter, onClick }: LogoProps) => {
+export const Logo = ({ isHeader, isFooter }: LogoProps) => {
   const logoClassName = classNames(' stroke-current fill-current', {
-    'w-[51px] h-[54px] xl:w-20 xl:h-20': isHeader,
-    'w-[123px] h-[130px]': isFooter,
+    'w-51px h-54px xl:w-20 xl:h-20': isHeader,
+    'w-123px h-130px': isFooter,
   });
 
   const LinkClassName = classNames(
-    'cursor-pointer inline-block  hover:text-[#508050] focus:text-[#508050] transition-all duration-300',
+    'cursor-pointer inline-block  hover:text-subtitle focus:text-subtitle transition-all duration-300',
     {
-      'text-[#224722]': isHeader,
+      'text-main': isHeader,
       'text-white': isFooter,
     },
   );
 
   return (
-    <Link href="/" className={LinkClassName} onClick={onClick}>
+    <Link href="/" className={LinkClassName}>
       <LogoIcon
         className={logoClassName}
         strokeMiterlimit="10"
