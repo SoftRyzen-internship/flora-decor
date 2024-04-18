36fc3a131c3ddf5643648f782632d22e7127de53
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-18 08:47:20 +0100

    added custom scroll bar

--- sections/Flowerpots/CustomScrollbar.tsx
+++ sections/Flowerpots/CustomScrollbar.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode } from 'react';
 import SimpleBar from 'simplebar-react';
+// import { Scrollbars } from 'react-custom-scrollbars-2';
 import 'simplebar/dist/simplebar.min.css';
 
 type ScrollProps = {
@@ -23,3 +24,27 @@ export const CustomScrollbar = ({ children }: ScrollProps) => {
     <SimpleBar style={{ width: '100%', height: '300px' }}>{children}</SimpleBar>
   );
 };
+
+{
+  /* react-custom-scrollbars-2*/
+}
+
+// export const CustomScrollbar = ({ children }: ScrollProps) => {
+//   return (
+//     <Scrollbars
+//       style={{ width: '100%', height: '100%' }}
+//       renderThumbVertical={({ style, ...props }) => (
+//         <div
+//           {...props}
+//           style={{
+//             ...style,
+//             backgroundColor: '#888',
+//             borderRadius: '5px',
+//           }}
+//         />
+//       )}
+//     >
+//       {children}
+//     </Scrollbars>
+//   );
+// };
