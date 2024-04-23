4460ef044f88d5a9f6b2a3d0a23e1fef45b997dd
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-23 11:26:32 +0100

    added hero.json

--- sections/Hero/Hero.tsx
+++ sections/Hero/Hero.tsx
@@ -2,18 +2,22 @@ import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
 import { Section } from '@/components/Section';
 
+import data from '@/data/hero.json';
+
 export const Hero = () => {
+  const { title, subtitle, btnText } = data;
+
   return (
     <Section sectionId="hero" variant="heroSection" className="hero">
       <Container>
         <h1 className="text-titleMd font-geologica text-main mb-[15px] md:mb-5 md:text-heroTitleMd xl:text-heroTitleXl xl:w-[558px]">
-          Оживіть свій простір
+          {title}
         </h1>
         <p className="text-subtitleXs font-geologica text-description md:text-subtitleMdCol mb-[30px] md:mb-10 md:text-subtitleMd md:w-[318px]">
-          Ексклюзивні квіти в вазонах для вашого дому або офісу!
+          {subtitle}
         </p>
         <Button isLink isDisabled={false} isOrderLink>
-          Замовити
+          {btnText}
         </Button>
       </Container>
     </Section>
