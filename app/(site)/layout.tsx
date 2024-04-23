459657cf141bdd7eee646ccd67c18b30bb20d28c
chemvic <chemvic79@gmail.com>
2024-04-23 13:35:44 +0300

    form reset fix

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,10 +1,23 @@
-import { Mulish, Geologica } from 'next/font/google';
 import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { Mulish, Geologica } from 'next/font/google';
 
 import { Header } from '@/layout/Header';
 import { Footer } from '@/layout/Footer';
 
+import metaInfo from '@/data/meta/home.json';
 import './globals.css';
+
+const { title, description, manifest, icons } = metaInfo;
+
+export const metadata: Metadata = {
+  title,
+  description,
+
+  manifest,
+
+  icons,
+};
 
 const mulish = Mulish({
   subsets: ['cyrillic'],
