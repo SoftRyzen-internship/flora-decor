74ff6b63a7e365ba29d0805161faf1e05cc35d4f
chemvic <chemvic79@gmail.com>
2024-04-23 18:41:10 +0300

    with meta

--- app/(site)/policy/page.tsx
+++ app/(site)/policy/page.tsx
@@ -1,23 +1,23 @@
-// import type { Metadata } from 'next';
+import type { Metadata } from 'next';
 
 import { Policy } from '@/sections/Policy';
 
 import { getPolicy } from '@/sanity/requests/getPolicy';
 
-// import metaInfo from '@/data/meta/policy.json';
+import metaInfo from '@/data/meta/policy.json';
 
-// const { title, slug, openGraph } = metaInfo;
+const { title, slug, openGraph } = metaInfo;
 
-// const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${slug}/` as string;
+const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${slug}/` as string;
 
-// export const metadata: Metadata = {
-//   metadataBase: new URL(baseUrl),
-//   title,
-//   alternates: {
-//     canonical: baseUrl,
-//   },
-//   openGraph: { ...openGraph, url: `${baseUrl}` },
-// };
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title,
+  alternates: {
+    canonical: baseUrl,
+  },
+  openGraph: { ...openGraph, url: `${baseUrl}` },
+};
 
 const page = async () => {
   const policy = await getPolicy();
