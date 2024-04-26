299883f73b0c4d37c2039b8e52b671a4681d5e88
VitaBabenko <babenkovita1988@gmail.com>
2024-04-26 12:08:04 +0300

    fix gallery

--- components/GalleryImgItem/GalleryImgItem.tsx
+++ components/GalleryImgItem/GalleryImgItem.tsx
@@ -4,16 +4,22 @@ import React from 'react';
 type GalleryImgItemProps = {
   img: any;
   alt: string;
+  width: number;
+  height: number;
 };
 
-export const GalleryImgItem: React.FC<GalleryImgItemProps> = ({ img, alt }) => {
+export const GalleryImgItem: React.FC<GalleryImgItemProps> = ({
+  img,
+  alt,
+  width,
+  height,
+}) => {
   return (
     <li className="object-cover aspect-ratio overflow-hidden md:w-[330px]">
       <Image
         src={img}
-        width={652}
-        height={615}
-        priority
+        width={width}
+        height={height}
         alt={alt}
         className="w-full h-full object-cover"
       />
