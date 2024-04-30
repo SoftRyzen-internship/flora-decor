219edceed5c09912f1e064b5387e7ec390d645ff
Nadiia Khoptiuk <holineinadia@gmail.com>
2024-04-30 14:14:13 +0300

    fix images import at Gallery

--- data/gallery.tsx
+++ data/gallery.tsx
@@ -1,67 +1,53 @@
-import img1 from '@/public/images/gallery/tablet/picture_1@2.jpg';
-import img2 from '@/public/images/gallery/tablet/picture_2@2.jpg';
-import img3 from '@/public/images/gallery/tablet/picture_3@2.jpg';
-import img4 from '@/public/images/gallery/tablet/picture_4@2.jpg';
-import img5 from '@/public/images/gallery/tablet/picture_5@2.jpg';
-import img6 from '@/public/images/gallery/tablet/picture_6@2.jpg';
-
-import img1Desktop from '@/public/images/gallery/desktop/picture_1@2.jpg';
-import img2Desktop from '@/public/images/gallery/desktop/picture_2@2.jpg';
-import img3Desktop from '@/public/images/gallery/desktop/picture_3@2.jpg';
-import img4Desktop from '@/public/images/gallery/desktop/picture_4@2.jpg';
-import img5Desktop from '@/public/images/gallery/desktop/picture_5@2.jpg';
-import img6Desktop from '@/public/images/gallery/desktop/picture_6@2.jpg';
-
 export const galleryImg = [
   {
-    img: img1,
+    img: '/images/gallery/tablet/picture_1@2.jpg',
     alt: 'вазон стоіть біля вікна',
   },
   {
-    img: img2,
+    img: '/images/gallery/tablet/picture_2@2.jpg',
     alt: 'вазон стоїть на полу',
   },
   {
-    img: img3,
+    img: '/images/gallery/tablet/picture_3@2.jpg',
     alt: 'вазони стоять на поличці',
   },
   {
-    img: img4,
+    img: '/images/gallery/tablet/picture_4@2.jpg',
     alt: 'вазони на фоні салатової стіни',
   },
   {
-    img: img5,
+    img: '/images/gallery/tablet/picture_5@2.jpg',
     alt: 'вазон на фоні зеленої стіни',
   },
   {
-    img: img6,
+    img: '/images/gallery/tablet/picture_6@2.jpg',
     alt: 'вазони стоять на столику біля дивану',
   },
 ];
 
 export const galleryImgDesktop = [
   {
-    img: img1Desktop,
+    img: '/images/gallery/desktop/picture_1@2.jpg',
     alt: 'вазон стоіть біля вікна',
   },
   {
-    img: img2Desktop,
+    img: '/images/gallery/desktop/picture_2@2.jpg',
     alt: 'вазон стоїть на полу',
   },
   {
-    img: img3Desktop,
+    img: '/images/gallery/desktop/picture_3@2.jpg',
     alt: 'вазони стоять на поличці',
   },
   {
-    img: img4Desktop,
+    img: '/images/gallery/desktop/picture_4@2.jpg',
     alt: 'вазони на фоні салатової стіни',
   },
   {
-    img: img5Desktop,
+    img: '/images/gallery/desktop/picture_5@2.jpg',
     alt: 'вазон на фоні зеленої стіни',
   },
   {
-    img: img6Desktop,
+    img: '/images/gallery/desktop/picture_6@2.jpg',
     alt: 'вазони стоять на столику біля дивану',
   },
 ];
